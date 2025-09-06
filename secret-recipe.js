27eddd2463fdf6904eb4a2613e2f53ffcb817767
nupur-gudigar <ngudigar@hawk.iit.edu>
2025-09-06 12:34:38 -0500

    secret-recipe

---
+++ secret-recipe.js
@@ -0,0 +1,23 @@
+
+
+// My favorite dish: Spicy Ramen 🍜
+// Nupur
+
+console.log("Ingredients:");
+console.log("🍜 1 packet of spicy garlic noodles");
+console.log("🥕🫑  1 cup of veggies: mini bell peppers, spring onion, chopped garlic");
+console.log("🍳 1 egg");
+console.log("🥬 1 leaf of bok choy (optional)\n");
+
+console.log("Instructions:");
+console.log("1. Cook the noodles until al dente. Drain and set aside.");
+console.log("2. In a large skillet, melt the butter over medium heat.and add chopped garlic until golden, and the egg and fry it.");
+console.log("3. Stir in the grated Parmesan cheese until the veggies are fried for the noodles");
+console.log("4. Add the cooked noodles to the veggies and toss to coat evenly.");
+console.log("5. Serve hot, garnished with fresh bok choy leaf if desired.");
+
+/* 
+Secret Ingredient 🤫:
+A squeeze of fresh lemon juice brightens the dish 
+and adds a subtle tang that complements the richness of the sauce.
+*/
